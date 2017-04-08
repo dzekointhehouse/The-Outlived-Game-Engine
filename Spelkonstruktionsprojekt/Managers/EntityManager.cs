0b5c2d8e3736179155728ecfce0a12567e949a34
optimusprime <s143522@student.hb.se>
2017-04-08 12:24:34 +0200

    entitymanager and a component

---
+++ Spelkonstruktionsprojekt/Managers/EntityManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using ZombieGame;
+
+namespace Spelkonstruktionsprojekt
+{
+    class EntityManager
+    {
+        private static EntityManager _entityManager;
+        private int _nextEntityId;
+        private Dictionary<int, Velocity> ExistingEntities;
+        public Dictionary<int, Velocity> Type { get; set; }
+
+
+        private EntityManager()
+        {
+            _nextEntityId = 0;
+        }
+
+        public static EntityManager GetEntityManager()
+        {
+            if (_entityManager != null)
+            {
+                return _entityManager;
+            }
+            else
+            {
+                _entityManager = new EntityManager();
+                return _entityManager;
+            }
+        }
+        public int GetNewEntity()
+        {
+            return _nextEntityId++;
+        }
+
+
+    }
+
+
+}
---
+++ Spelkonstruktionsprojekt/Velocity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace ZombieGame
+{
+    class Velocity
+    {
+        static string ComponentName = "Velocity";
+
+        public Vector2 velocity { get; set; }
+
+
+    }
+}
