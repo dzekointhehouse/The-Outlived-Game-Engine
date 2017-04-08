a49652edefd1661b4e95f0eb6eb1aebe14814f22
augustalex <augustalexandersson@gmail.com>
2017-04-08 12:43:01 +0200

    You can now create new Systems through statically added System types through the SystemManager

---
+++ Spelkonstruktionsprojekt/ZEngine/SystemManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Systems
+{
+    public class SystemManager
+    {
+        private readonly Dictionary<string, Type> _systems = new Dictionary<string, Type>()
+        {
+            { "Render", typeof(RenderSystem) }
+        };
+
+        public Type GetSystem(string systemName)
+        {
+            if (!_systems.ContainsKey(systemName))
+            {
+                throw new Exception("No such system.");
+            }
+            else
+            {
+                return _systems[systemName];
+            }
+        }
+            
+        public T GetSystem<T>() where T : new()
+        {
+            if (this._systems.ContainsValue(typeof(T)))
+            {
+                Type type = this._systems.First(entry => entry.Value == typeof(T)).Value;
+                return (T) Activator.CreateInstance(type);
+            }
+            else
+            {
+                throw new Exception("No such system exist.");
+            }
+        }
+    }
+}
