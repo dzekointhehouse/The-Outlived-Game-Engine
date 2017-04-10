b3fd1bff44fef119203fcba354766b7a0593454b
Administrator <Administrator@COMPUTER>
2017-04-10 12:21:27 +0200

    Added PickupComponent

---
+++ Spelkonstruktionsprojekt/ZEngine/Components/PickupComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spelkonstruktionsprojekt.ZEngine.Components
+{
+    /*
+     * This component is for medkits and ammo pickups, has an enum so we know what type it is.
+     */
+    class PickupComponent
+    {
+        public enum PickupEventType
+        {
+            Health,
+            Ammo
+        }
+    }
+}
