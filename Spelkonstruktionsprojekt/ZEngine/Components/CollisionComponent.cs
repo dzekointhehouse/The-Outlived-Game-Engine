871b88fe3ade1cbe362b83ccaf1ae16a62d27494
carlitos79 <carlitos792001@yahoo.es>
2017-04-12 10:24:01 +0200

    collision

--- Spelkonstruktionsprojekt/ZEngine/Components/CollisionComponent.cs
+++ Spelkonstruktionsprojekt/ZEngine/Components/CollisionComponent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,13 @@ namespace ZEngine.Components.CollisionComponent
 {
     class CollisionComponent : IComponent
     {
-
+        private Vector2 spritePosition;
+        private Rectangle spriteBoundingRectangle;
+        private BoundingSphere spriteBoundingSphere;
+        private const int spriteMoveSpeed = 5;
+        private Vector2 spriteOrigin;
+        private Vector3 spriteCenter;
+        private float spriteRadius;
+        private Boolean isCollidable;
     }
 }
