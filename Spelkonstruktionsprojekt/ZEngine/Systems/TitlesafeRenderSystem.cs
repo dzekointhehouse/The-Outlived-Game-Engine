3b1a559e324b3bdd23b09bf203c285b927dad275
Optimusprime <Optimusprime@BB-8>
2017-04-16 11:21:56 +0200

    updates on my stuff, and a new component

--- Spelkonstruktionsprojekt/ZEngine/Systems/TitlesafeRenderSystem.cs
+++ Spelkonstruktionsprojekt/ZEngine/Systems/TitlesafeRenderSystem.cs
@@ -12,13 +12,18 @@ using ZEngine.Wrappers;
 
 namespace Spelkonstruktionsprojekt.ZEngine.Systems
 {
+    // This system is used for rendering the HUD, those entities
+    // that have instances ofcomponents like health or ammo will 
+    // be able to se the component data on the screen when playing.
+    // This can be used to that the player always can see the status
+    // of his player. (everything drawn here is and should be titlesafe) 
     class TitlesafeRenderSystem : ISystem
     {
         public static string SystemName = "TitlesafeRender";
-        private ComponentManager _componentManager = ComponentManager.Instance;
         private GameDependencies _gameDependencies;
 
-
+        // This draw method is used to start the system process.
+        // it uses DrawTitleSafe to draw the components.
         public void Draw(GameDependencies gameDependencies)
         {
             this._gameDependencies = gameDependencies;
@@ -28,24 +33,55 @@ namespace Spelkonstruktionsprojekt.ZEngine.Systems
             _gameDependencies.SpriteBatch.End();
         }
 
+
+        // DrwaTitleSafe gets all the components and draws them in a
+        // correct format, so the data will sortet so that each entity will
+        // have it's own row, and each component will be sorted by column.
         private void DrawTitleSafe()
         {
             var graphics = _gameDependencies.GraphicsDeviceManager.GraphicsDevice;
             var titlesafearea = graphics.Viewport.TitleSafeArea;
 
-            // Loading necessary components
-            var renderable = _componentManager.GetEntitiesWithComponent<HealthComponent>();
+            var playerComponents = ComponentManager.Instance.GetEntitiesWithComponent<PlayerComponent>();
+            //var healthComponents = ComponentManager.Instance.GetEntitiesWithComponent<HealthComponent>();
+            //var renderable = ComponentManager.Instance.GetEntitiesWithComponent<AmmoComponent>();
 
             // We save the previous text height so we can stack
             // them on top of eachother.
             var previousHeight = 0f;
 
-            foreach (var instance in renderable)
-            {
-                var g = _gameDependencies.GameContent as ContentManager;
-                var spriteFont = g.Load<SpriteFont>("Healthfont");
+            var g = _gameDependencies.GameContent as ContentManager;
+            // Need to let the user decide
+            var spriteFont = g.Load<SpriteFont>("Healthfont");
 
-                string text = instance.Value.CurrentHealth + " / " + instance.Value.MaxHealth;
+            foreach (var playerInstance in playerComponents)
+            {
+                //var g = _gameDependencies.GameContent as ContentManager;
+                //var spriteFont = g.Load<SpriteFont>("Healthfont");
+
+                //string text = instance.Value.CurrentHealth + " | " + instance.Value.MaxHealth;
+
+                //var textHeight = spriteFont.MeasureString(text).Y;
+
+                //var xPosition = titlesafearea.Width - spriteFont.MeasureString(text).X - 10;
+                //var yPosition = titlesafearea.Height - textHeight - previousHeight;
+                //previousHeight = textHeight;
+
+                //var position = new Vector2(xPosition, yPosition);
+                //_gameDependencies.SpriteBatch.DrawString(spriteFont, text, position, Color.White);
+
+                string text = playerInstance.Value.Name;
+
+                if (ComponentManager.Instance.EntityHasComponent<HealthComponent>(playerInstance.Key))
+                {
+                    var health = ComponentManager.Instance.GetEntityComponent<HealthComponent>(playerInstance.Key);
+                    text = text + ": " + health.CurrentHealth + "|" + health.MaxHealth;
+                }
+                if (ComponentManager.Instance.EntityHasComponent<AmmoComponent>(playerInstance.Key))
+                {
+                    var ammo = ComponentManager.Instance.GetEntityComponent<AmmoComponent>(playerInstance.Key);
+                    text = text + " Ammo: " + ammo.Amount;
+                }
 
                 var textHeight = spriteFont.MeasureString(text).Y;
 
@@ -55,6 +91,7 @@ namespace Spelkonstruktionsprojekt.ZEngine.Systems
 
                 var position = new Vector2(xPosition, yPosition);
                 _gameDependencies.SpriteBatch.DrawString(spriteFont, text, position, Color.White);
+
             }
         }
     }
