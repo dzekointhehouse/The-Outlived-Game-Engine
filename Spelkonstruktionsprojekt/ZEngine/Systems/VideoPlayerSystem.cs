607ffc1b4a736b24038e5f456aa747b0663b2d35
optimusprime94 <s143522@student.hb.se>
2017-04-16 23:17:52 +0200

    just checking something

---
+++ Spelkonstruktionsprojekt/ZEngine/Systems/VideoPlayerSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Media;
+
+namespace Spelkonstruktionsprojekt.ZEngine.Systems
+{
+    class VideoPlayerSystem
+    {
+
+        private VideoPlayer videoPlayer;
+
+        public bool IsDonePlaying()
+        {
+                return videoPlayer.State == MediaState.Stopped;  
+        }
+    }
+}
