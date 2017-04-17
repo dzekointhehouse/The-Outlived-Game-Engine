d74bebd5d9c048d4399ccad5f7d5858ac4bde54f
optimusprime94 <dzekson94@gmail.com>
2017-04-17 08:37:27 +0200

    videosystem

--- Spelkonstruktionsprojekt/ZEngine/Systems/VideoPlayerSystem.cs
+++ Spelkonstruktionsprojekt/ZEngine/Systems/VideoPlayerSystem.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
+using ZEngine.Wrappers;
 
 namespace Spelkonstruktionsprojekt.ZEngine.Systems
 {
@@ -15,6 +18,33 @@ namespace Spelkonstruktionsprojekt.ZEngine.Systems
         public bool IsDonePlaying()
         {
                 return videoPlayer.State == MediaState.Stopped;  
+
+
+        }
+
+        public void Start()
+        {
+            if (videoPlayer.State == MediaState.Stopped)
+            {
+               // videoPlayer.Play(video);
+            }
+
+        }
+
+        public void Draw(GameDependencies gameDependencies)
+        {
+            var viewport = gameDependencies.GraphicsDeviceManager.GraphicsDevice.Viewport;
+            Texture2D videoTexture = null;
+
+            if (!IsDonePlaying())
+                videoTexture = videoPlayer.GetTexture();
+
+            if (videoTexture != null)
+            {
+                gameDependencies.SpriteBatch.Begin();
+                gameDependencies.SpriteBatch.Draw(videoTexture, new Rectangle(0, 0, (int)viewport.X, (int)viewport.Y), Color.White);
+                gameDependencies.SpriteBatch.End();
+            }
         }
     }
 }
