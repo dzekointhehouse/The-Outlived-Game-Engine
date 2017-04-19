03ccaaaeb5b25a95271ddf469fb98a2ef19289fc
augustalex <augustalexandersson@gmail.com>
2017-04-19 15:51:22 +0200

    Example Button class for front end

---
+++ Spelkonstruktionsprojekt/Zenu/Button.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using ZEngine.Components;
+using ZEngine.Managers;
+
+namespace Spelkonstruktionsprojekt.Zenu
+{
+    public class Button
+    {
+        public Button(string text, Rectangle rectangle)
+        {
+            var entityId = EntityManager.GetEntityManager().NewEntity();
+            var renderComponent = new RenderComponentBuilder()
+                .Position(rectangle.X, rectangle.Y, 800)
+                .Dimensions(rectangle.Width, rectangle.Height)
+                .Build();
+            var spriteComponent = new SpriteComponent()
+            {
+                SpriteName = "dot"
+            };
+            ComponentManager.Instance.AddComponentToEntity(renderComponent, entityId);
+            ComponentManager.Instance.AddComponentToEntity(spriteComponent, entityId);
+        }
+    }
+}
