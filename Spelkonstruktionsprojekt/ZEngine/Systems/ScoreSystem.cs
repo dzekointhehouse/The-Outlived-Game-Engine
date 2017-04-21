9cca30a3116559a1af09319fd60f6d0ba24d2ffc
Administrator <Administrator@COMPUTER>
2017-04-21 12:37:59 +0200

    Created ScoreComponent and basic ScoreSystem.

---
+++ Spelkonstruktionsprojekt/ZEngine/Systems/ScoreSystem.cs
@@ -0,0 +1,30 @@
+﻿using Spelkonstruktionsprojekt.ZEngine.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZEngine.Managers;
+
+namespace Spelkonstruktionsprojekt.ZEngine.Systems
+{
+    class ScoreSystem : ISystem
+    {
+        public void increaseEntityScore(int entityId, int points)
+        {
+            var scoreComponent = (ScoreComponent) ComponentManager.Instance.GetEntityComponentOrDefault(typeof(ScoreComponent), entityId);
+            scoreComponent.score += points;
+        }
+
+        public int totalScore()
+        {
+            int total = 0;
+            var scoreEntities = ComponentManager.Instance.GetEntitiesWithComponent<ScoreComponent>();
+            foreach (int key in scoreEntities.Keys)
+            {
+                total += scoreEntities[key].score;
+            }
+            return total;
+        }
+    }
+}
