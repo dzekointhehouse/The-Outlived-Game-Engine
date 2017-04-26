d971b793682eec26f1074968cacd27a0074dbb94
augustalex <augustalexandersson@gmail.com>
2017-04-26 18:53:43 +0200

    removed items from gitignore

---
+++ Spelkonstruktionsprojekt/ZEngine/Components/MoveComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ZEngine.Components
+{
+    public class MoveComponent : IComponent
+    {
+        public Vector2 PreviousPosition { get; set; }
+        public float Direction { get; set; } = 0;
+        public float Speed = 0;
+        public double CurrentAcceleration = 0;
+        public double AccelerationSpeed = 10;
+        public double BackwardsPenaltyFactor = 0.5;
+        public float MaxVelocitySpeed = 0;
+        public double RotationMomentum = 0;
+        public double RotationSpeed = 0;
+    }
+}
