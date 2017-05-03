91c403ca80bcac3cda4b54ce21a04ddaf3fa814d
augustalex <augustalexandersson@gmail.com>
2017-05-03 07:46:02 +0200

    Added files

---
+++ Spelkonstruktionsprojekt/ZEngine/Components/SpriteAnimation/SpriteAnimationBindings.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using ZEngine.Components;
+
+namespace Spelkonstruktionsprojekt.ZEngine.Components.SpriteAnimation
+{
+    public class SpriteAnimationBindings : IComponent
+    {
+        public List<SpriteAnimationBinding> Bindings = new List<SpriteAnimationBinding>();
+    }
+
+    public class SpriteAnimationBinding
+    {
+        public List<State> StateConditions { get; set; } = new List<State>();
+        public Point StartPosition { get; set; }= default(Point);
+        public Point EndPosition { get; set; }= default(Point);
+        public double FrameLength { get; set; } = 16;
+    }
+
+    public class SpriteAnimationBindingBuilder
+    {
+        private readonly SpriteAnimationBinding _spriteAnimationBinding = new SpriteAnimationBinding();
+
+        public SpriteAnimationBindingBuilder StateConditions(params State[] stateConditions)
+        {
+            _spriteAnimationBinding.StateConditions = stateConditions.ToList();
+            return this;
+        }
+
+        public SpriteAnimationBindingBuilder Positions(Point startPosition, Point endPosition)
+        {
+            _spriteAnimationBinding.StartPosition = startPosition;
+            _spriteAnimationBinding.EndPosition = endPosition;
+            return this;
+        }
+
+        public SpriteAnimationBindingBuilder Length(double lengthInMilliseconds)
+        {
+            _spriteAnimationBinding.FrameLength = lengthInMilliseconds;
+            return this;
+        }
+
+        public SpriteAnimationBinding Build()
+        {
+            return _spriteAnimationBinding;
+        }
+    }
+
+    public class SpriteAnimationBindingsBuilder
+    {
+        private readonly SpriteAnimationBindings _spriteAnimationBindings = new SpriteAnimationBindings();
+
+        public SpriteAnimationBindingsBuilder Binding(SpriteAnimationBinding binding)
+        {
+            _spriteAnimationBindings.Bindings.Add(binding);
+            return this;
+        }
+
+        public SpriteAnimationBindings Build()
+        {
+            return _spriteAnimationBindings;
+        }
+    }
+}
---
+++ Spelkonstruktionsprojekt/ZEngine/Components/StateComponent.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Spelkonstruktionsprojekt.ZEngine.Managers;
+using ZEngine.Components;
+using ZEngine.EventBus;
+using ZEngine.Managers;
+
+namespace Spelkonstruktionsprojekt.ZEngine.Components
+{
+    public class StateComponent : IComponent
+    {
+        public HashSet<State> State { get; set; } = new HashSet<State>();
+    }
+
+    public enum State
+    {
+        WalkingForward,
+        WalkingBackwards,
+        Shooting,
+        Dead
+    }
+
+    public static class StateManager
+    {
+        private static readonly EventBus EventBus = EventBus.Instance;
+
+        public static void TryAddState(int entityId, State state, double currentTime)
+        {
+            var stateComponent = ComponentManager.Instance.GetEntityComponentOrDefault<StateComponent>(entityId);
+            stateComponent?.State.Add(state);
+            PublishStateChangeEvent(entityId, currentTime);
+        }
+
+        public static void TryRemoveState(int entityId, State state, double currentTime)
+        {
+            var stateComponent = ComponentManager.Instance.GetEntityComponentOrDefault<StateComponent>(entityId);
+            stateComponent?.State.Remove(state);
+            PublishStateChangeEvent(entityId, currentTime);
+        }
+
+        public static void PublishStateChangeEvent(int entityId, double currentTime)
+        {
+            EventBus.Publish(
+                "StateChanged",
+                new StateChangeEventBuilder()
+                    .Entity(entityId)
+                    .Time(currentTime).Build()
+            );
+        }
+    }
+}
