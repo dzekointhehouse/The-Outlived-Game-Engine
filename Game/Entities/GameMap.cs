ba7213cc100376c5a36eef4f475050eaa7c58313
optimusprime94 <dzekson94@gmail.com>
2017-05-05 22:51:25 +0200

    god damn was not added again

---
+++ Game/Entities/GameMap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spelkonstruktionsprojekt.ZEngine.Helpers;
+using Spelkonstruktionsprojekt.ZEngine.Helpers.DefaultMaps;
+
+namespace Game.Entities
+{
+    public class GameMap
+    {
+
+
+
+        public void SetupGameMap(MapType map)
+        {
+            switch (map)
+            {
+                    case MapType.BlockWorld:
+                        var tileTypes = new Dictionary<int, string>();
+                        //tileTypes.Add(0, "blue64");
+                        tileTypes.Add(4, "grass");
+                        //tileTypes.Add(2, "red64");
+                        //tileTypes.Add(4, "yellowwall64");
+                        MapHelper mapcreator = new MapHelper(tileTypes);
+                        mapcreator.CreateMapTiles(MapPack.Blockworld, 100);
+                        break;
+            }
+
+
+
+
+        }
+    }
+
+    public enum MapType
+    {
+        BlockWorld,
+        Downtown,
+        LastSurvivors
+    }
+
+}
