648934ebfe4b3479b126a0e70bd71c7a83c122b6
optimusprime94 <dzekson94@gmail.com>
2017-05-12 23:33:21 +0200

    moved files

--- Game/Menu/States/GameModeMenu.cs
+++ Game/Menu/States/GameModeMenu.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using ZEngine.Wrappers;
 
-namespace Game.Menu
+namespace Game.Menu.States
 {
     class GameModeMenu : IMenu
     {
