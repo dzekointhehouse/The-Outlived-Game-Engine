ac1f2fd7cc29846d6abd2c691eb56acb9b01fe09
Administrator <Administrator@COMPUTER>
2017-05-16 15:12:38 +0200

    HighScoreSystem

---
+++ Spelkonstruktionsprojekt/ZEngine/Components/HighScoreComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZEngine.Components;
+
+namespace Spelkonstruktionsprojekt.ZEngine.Components
+{
+    class HighScoreComponent : IComponent
+    {
+        public string[] name = new string[10];
+        public string[] score = new string[10];
+        public string path;
+    }
+}
