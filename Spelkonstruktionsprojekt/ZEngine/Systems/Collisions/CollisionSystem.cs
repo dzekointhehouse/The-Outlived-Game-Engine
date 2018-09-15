dcbc321d6051dfab096fcd0b762c99417d2c4340
Elvir Dzeko <dzekson94@gmail.com>
2018-09-15 19:52:38 +0200

    Phasing out pixel stuff

--- Spelkonstruktionsprojekt/ZEngine/Systems/Collisions/CollisionSystem.cs
+++ Spelkonstruktionsprojekt/ZEngine/Systems/Collisions/CollisionSystem.cs
@@ -32,7 +32,6 @@ namespace ZEngine.Systems
 
         private readonly ComponentManager ComponentManager = ComponentManager.Instance;
 
-        private Dictionary<Tuple<string, Point>, Color[]> cache;
         private Dictionary<uint, bool> IsAI;
         private Dictionary<uint, bool> IsBullet;
         private Dictionary<uint, int> CallCount;
@@ -43,7 +42,6 @@ namespace ZEngine.Systems
             IsAI = new Dictionary<uint, bool>();
             IsBullet = new Dictionary<uint, bool>();
             CallCount = new Dictionary<uint, int>();
-            cache = new Dictionary<Tuple<string, Point>, Color[]>();
             QuadTree = new QuadTree(ComponentManager.Instance);
         }
 
@@ -52,66 +50,6 @@ namespace ZEngine.Systems
         }
 
 
-        
-        private int cacheMisses = 0;
-        
-        
-        //TODO Should do this on game load
-        public Color[] TextureCache(SpriteComponent spriteComponent, uint entityId)
-        {
-            Color[] data;
-            var key = new Tuple<string, Point>(spriteComponent.SpriteName, spriteComponent.Position);
-            var status = cache.TryGetValue(key, out data);
-            if (!status)
-            {
-                int x, y;
-
-                var animationComponent =
-                    ComponentManager.GetEntityComponentOrDefault<SpriteAnimationComponent>(entityId);
-                if (animationComponent == null || (animationComponent.CurrentAnimatedState == null &&
-                                                   animationComponent.NextAnimatedState == null))
-                {
-                    x = spriteComponent.Position.X;
-                    y = spriteComponent.Position.Y;
-                }
-                else
-                {
-                    SpriteAnimationBinding animationState;
-                    if (animationComponent.CurrentAnimatedState != null)
-                    {
-                        animationState = animationComponent.CurrentAnimatedState;
-                    }
-                    else
-                    {
-                        animationState = animationComponent.NextAnimatedState;
-                    }
-
-                    x = animationState.StartPosition.X;
-                    y = animationState.StartPosition.Y;
-                }
-                data =
-                    new Color[spriteComponent.TileWidth * spriteComponent.TileHeight];
-                spriteComponent.Sprite.GetData(0,
-                    new Rectangle(
-                        x,
-                        y,
-                        spriteComponent.TileWidth,
-                        spriteComponent.TileHeight),
-                    data, 0, data.Length);
-                cache[key] = data;
-                if (PROFILING_CACHE)
-                {
-                    cacheMisses++;
-                }
-            }
-            return data;
-        }
-
-        private const bool PROFILING_COLLISIONS = false;
-        private const bool PROFILING_COLLISIONS_DEEP = false;
-        private const bool PROFILING_CACHE = false;
-        private const bool PROFILING_CALL_COUNT = false;
-        
         private int movingEntity = 0;
         private int stillEntity = 0;
         private int detectCollision = 0;
@@ -119,22 +57,12 @@ namespace ZEngine.Systems
 
         public async Task Update()
         {
-            Stopwatch timer;
-            if (PROFILING_COLLISIONS)
-            {
-                timer = Stopwatch.StartNew();
-            }
-            //
+
             QuadTree.CreateTree(
                 ComponentManager.GetEntitiesWithComponent(typeof(CollisionComponent)).Keys,
                 new Rectangle(0, 0, 50000, 50000)
             );
 
-            if (PROFILING_COLLISIONS)
-            {
-                Debug.WriteLine("QUAD TREE CONSTR." + timer.ElapsedTicks);
-                timer = Stopwatch.StartNew();
-            }
 
             var tasks = new List<Task>();
             foreach (var movingEntity in QuadTree.Entities())
@@ -163,37 +91,13 @@ namespace ZEngine.Systems
                 
                     if (IsBothAI(movingEntity.Key, stillEntity)) continue;
                     if (IsStillBullet(stillEntity)) continue;
-//                    await CollisionDetection(movingEntity, stillEntity);
+
                     tasks.Add(CollisionDetection(movingEntity.Key, stillEntity));
                 }
             }
-//            if (PROFILING_COLLISIONS)
-//            {
-//                Debug.WriteLine("QUAD TRAVERSE TOTAL " + timer.ElapsedTicks);
-//                timer = Stopwatch.StartNew();
-//            }
+
             await Task.WhenAll(tasks);
-            if (PROFILING_COLLISIONS)
-            {
-                Debug.WriteLine("COL. TOTAL " + timer.ElapsedTicks);
-            }
-            if (PROFILING_CACHE)
-            {
-                Debug.WriteLine("END OF COL.DET. MISSES: " + cacheMisses);
-                cacheMisses = 0;
-            }
-            if (PROFILING_CALL_COUNT)
-            {
-                Debug.WriteLine("MOVING: " + this.movingEntity + ", STILL: " + this.stillEntity + ", DETECT: " + this.detectCollision + ", COL: " + this.entitiesCollide);
-                int count = 0;
-                foreach (var e in CallCount)
-                {
-                    Debug.WriteLine("ID: " + e.Key + " COUNT: " + e.Value);
-                    count += e.Value;
-                }
-                Debug.WriteLine("REPETITION AVERAGE: " + count / CallCount.Count);
-                Debug.WriteLine("");
-            }
+
             this.movingEntity = 0;
             this.stillEntity = 0;
             this.detectCollision = 0;
@@ -238,8 +142,6 @@ namespace ZEngine.Systems
 
         private async Task CollisionDetection(uint movingEntity, uint stillEntity)
         {
-            this.detectCollision++;
-            Stopwatch timer;
             var movingCollision =
                 ComponentManager.GetEntityComponentOrDefault<CollisionComponent>(movingEntity);
             var areaComponent =
@@ -247,10 +149,6 @@ namespace ZEngine.Systems
 
             if (areaComponent != null)
             {
-                if (PROFILING_COLLISIONS_DEEP)
-                {
-                    timer = Stopwatch.StartNew();
-                }
                 var isPlayer = ComponentManager.EntityHasComponent<PlayerComponent>(movingEntity);
                 if (isPlayer)
                 {
@@ -267,44 +165,17 @@ namespace ZEngine.Systems
                         areaComponent.Inhabitants.Remove(movingEntity);
                     }
                 }
-                if (PROFILING_COLLISIONS_DEEP)
-                {
-                    var time = timer.ElapsedTicks;
-                    if (time > 5000)
-                    {
-                        Debug.WriteLine("INSIDE MOVING " + movingEntity + "/STILL " + stillEntity +
-                                        " Time: " + time);
-                        ComponentManager.Debug_ListComponentsForEntity(movingEntity);
-                        ComponentManager.Debug_ListComponentsForEntity(stillEntity);
-                    }
-                }
             }
             else
             {
-                if (PROFILING_COLLISIONS_DEEP)
-                {
-                    timer = Stopwatch.StartNew();
-                }
+
                 if (EntitiesCollide(movingEntity, stillEntity))
                 {
                     this.entitiesCollide++;
                     movingCollision.Collisions.Add(stillEntity);
                     //TODO might be that we need to add collision id to stillEntity as well
                 }
-                if (PROFILING_COLLISIONS_DEEP)
-                {
-                    var time = timer.ElapsedTicks;
-                    if (time > 0)
-                    {
-                        Debug.WriteLine("COLLIDED MOVING " + movingEntity + "/STILL " + stillEntity +
-                                        " Time: " + time);
-//                        Debug.WriteLine("QUAD NODE SIZE " + movingEntity.Item2.Bounds + " PERM.ELEMENTS: " +
-//                                        movingEntity.Item2.PermanentStillEntities.Count + " TEMP.ELEMENTS: " +
-//                                        movingEntity.Item2.TempStillEntitiesCount);
-                        ComponentManager.Debug_ListComponentsForEntity(movingEntity);
-                        ComponentManager.Debug_ListComponentsForEntity(stillEntity);
-                    }
-                }
+
             }
         }
 
@@ -365,24 +236,6 @@ namespace ZEngine.Systems
             var stillSpriteComponent = ComponentManager.GetEntityComponentOrDefault<SpriteComponent>(stillEntity);
             if (stillSpriteComponent == null) return false;
 
-            if (PROFILING)
-            {
-                timer.Stop();
-                Debug.WriteLine("GETTING DATA: " + timer.ElapsedTicks);
-                timer = Stopwatch.StartNew();
-            }
-
-            var colorA = TextureCache(movingSpriteComponent, movingEntity);
-
-            var colorB = TextureCache(stillSpriteComponent, stillEntity);
-
-            if (PROFILING)
-            {
-                timer.Stop();
-                Debug.WriteLine("TEXTURE DATA: " + timer.ElapsedTicks);
-                timer = Stopwatch.StartNew();
-            }
-
             var matrixA =
                 Matrix.CreateTranslation(new Vector3(
                     (float) (-movingSpriteComponent.TileWidth * 0.5),
@@ -404,13 +257,6 @@ namespace ZEngine.Systems
                     1) *
                 Matrix.CreateRotationZ(stillEntityAngle) *
                 Matrix.CreateTranslation(stillPositionComponent.Position.X, stillPositionComponent.Position.Y, 0f);
-
-            if (PROFILING)
-            {
-                timer.Stop();
-                Debug.WriteLine("MATRIX SETUP: " + timer.ElapsedTicks);
-                timer = Stopwatch.StartNew();
-            }
 
             var movingEntityCollisionBounds =
                 CalculateCollisionBounds(
@@ -441,37 +287,11 @@ namespace ZEngine.Systems
 
             if (movingEntityCollisionBounds.Intersects(stillEntityCollisionBounds))
             {
-                if (PROFILING)
-                {
-                    timer.Stop();
-                    Debug.WriteLine("BOUNDING BOXES: " + timer.ElapsedTicks);
-                    timer = Stopwatch.StartNew();
-                }
-                if (IntersectPixs(matrixA,
-                    movingSpriteComponent.TileWidth, movingSpriteComponent.TileHeight, colorA,
-                    matrixB, stillSpriteComponent.TileWidth, stillSpriteComponent.TileHeight,
-                    colorB))
-                {
-                    if (PROFILING)
-                    {
-                        timer.Stop();
-                        Debug.WriteLine("PIXEL TRUE: " + timer.ElapsedTicks);
-                    }
-                    return true;
-                }
 
-                if (PROFILING)
-                {
-                    timer.Stop();
-                    Debug.WriteLine("PIXEL FALSE: " + timer.ElapsedTicks);
-                }
-                return false;
+                return true;
+
             }
-            if (PROFILING)
-            {
-                timer.Stop();
-                Debug.WriteLine("BOUNDING BOXES [NO INTERSECT]: " + timer.ElapsedTicks);
-            }
+
             return false;
         }
 
@@ -498,69 +318,5 @@ namespace ZEngine.Systems
                 (int) (max.Y - min.Y));
         }
 
-        public static bool IntersectPixs(
-            Matrix transformA, int aWidth, int aHeight, Color[] dataA,
-            Matrix transformB, int bWidth, int bHeight, Color[] dataB)
-        {
-            // Calculate a matrix which transforms from A's local space into
-            // world space and then into B's local space
-            Matrix transformAToB = transformA * Matrix.Invert(transformB);
-
-            // When a point moves in A's local space, it moves in B's local space with a
-            // fixed direction and distance proportional to the movement in A.
-            // This algorithm steps through A one pixel at a time along A's X and Y axes
-            // Calculate the analogous steps in B:
-            //     Vector3 xStep = Vector3.Transform(new Vector3(1, 0, 0), transformAToB);
-            //     Vector3 yStep = Vector3.Transform(new Vector3(0, 1, 0), transformAToB);
-
-            Vector2 stepX = Vector2.TransformNormal(Vector2.UnitX, transformAToB);
-            Vector2 stepY = Vector2.TransformNormal(Vector2.UnitY, transformAToB);
-
-            // Calculate the top left corner of A in B's local space
-            // This variable will be reused to keep track of the start of each row
-            Vector2 yPosInB = Vector2.Transform(Vector2.Zero, transformAToB);
-
-            // For each row of pixels in A
-            for (int yA = 0; yA < aHeight; yA++)
-            {
-                // Start at the beginning of the row
-                Vector2 posInB = yPosInB;
-
-                // For each pixel in this row
-                for (int xA = 0; xA < aWidth; xA++)
-                {
-                    // Round to the nearest pixel
-                    int xB = (int) Math.Round(posInB.X);
-                    int yB = (int) Math.Round(posInB.Y);
-
-                    // If the pixel lies within the bounds of B
-                    if (0 <= xB && xB < bWidth &&
-                        0 <= yB && yB < bHeight)
-                    {
-                        // Get the colors of the overlapping pixels
-                        Color colorA = dataA[xA + yA * aWidth];
-                        Color colorB = dataB[xB + yB * bWidth];
-
-                        // If both pixels are not completely transparent,
-                        if (colorA.A != 0 && colorB.A != 0)
-                        {
-                            // then an intersection has been found
-                            return true;
-                        }
-                    }
-                    // Update to the next pixel in the row
-                    posInB += stepX;
-                }
-                // Update to the next row
-                yPosInB += stepY;
-            }
-            // No intersection found
-            return false;
-        }
-
-        public void ClearCache()
-        {
-            cache.Clear();
-        }
     }
 }
